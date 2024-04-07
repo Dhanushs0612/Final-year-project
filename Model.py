325e03539a387012b5ef246b9705e39ab65ac517
bothubeec <310622104153@eec.srmrmp.edu.in>
2024-04-07 20:38:32 +0530

    Updated

--- Model.py
+++ Model.py
@@ -8,20 +8,14 @@ import io
 
 app = Flask(__name__)
 
-# Load the pre-trained model
 model = tf.keras.models.load_model('C:/Users/USER/Downloads/model3.h5')
 
 def predict_lung_cancer(image_path):
-    # Load and preprocess the image
     img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, (256, 256))
     img = np.array([img]).reshape(-1, 256, 256, 1) / 255.0
-
-    # Make prediction
     prediction = model.predict(img)
     prediction_class = np.argmax(prediction)
-
-    # Map prediction to class label
     if prediction_class == 0:
         return "Benign case"
     elif prediction_class == 1:
@@ -41,13 +35,10 @@ def index():
             return jsonify({"error": "No selected file"}), 400
         
         if file:
-            # Create a unique filename for the uploaded image
             upload_dir = 'uploads'
             os.makedirs(upload_dir, exist_ok=True)
             image_path = os.path.join(upload_dir, file.filename)
             file.save(image_path)
-
-            # Make prediction using the uploaded image
             prediction = predict_lung_cancer(image_path)
 
             return render_template('model.html', prediction=prediction, filename=file.filename)
